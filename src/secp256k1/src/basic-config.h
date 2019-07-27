e31790c9a37254d21f01af8cef878fbdf4e7610a
bigdaveakers <3410330+bigdaveakers@users.noreply.github.com>
2019-07-27 19:03:18 +0100

    Add files via upload
    Groth

--- src/secp256k1/src/basic-config.h
+++ src/secp256k1/src/basic-config.h
@@ -28,5 +28,6 @@
 #define USE_FIELD_10X26 1
 #define USE_SCALAR_8X32 1
 
-#endif // USE_BASIC_CONFIG
-#endif // _SECP256K1_BASIC_CONFIG_
+#endif /* USE_BASIC_CONFIG */
+
+#endif /* SECP256K1_BASIC_CONFIG_H */
